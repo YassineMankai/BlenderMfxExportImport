14ca885aee3f1f1f99288cc72b46cbf69d3b2ebd
YassineMankai <yassine.mankai@outlook.com>
2022-09-11 22:37:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 graphName = 'test1'
+path = ''
 graph = D.node_groups[graphName]
 import sys, os
-os.chdir('D:/BlenderMfxExportImport/')
-sys.path.append('D:/BlenderMfxExportImport/')
+os.chdir('C:/Users/yassi/Desktop/Projects/OpenMFX/BlenderMfxExportImport/')
+sys.path.append('C:/Users/yassi/Desktop/Projects/OpenMFX/BlenderMfxExportImport/')
 
 from  geometryNodeToJSON import *
 exportToJSON(graph)
